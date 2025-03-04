17a418ace776d72d8949c65fa3506d236cd363cd
MViscardi-UCSC <mviscard@ucsc.edu>
2025-03-04 10:47:09 -0800

    add a check for samtools being ready to use

--- andromeda/__init__.py
+++ andromeda/__init__.py
@@ -13,8 +13,7 @@ if not shutil.which("umi_tools"):
     log.warning(
         "umi_tools not found in PATH, please install it to use the UMI grouping module."
     )
-for program in ["samtools", "bcftools"]:
-    if not shutil.which(program):
-        log.warning(
-            f"{program} not found in PATH, please install it to use the BAM tools module."
-        )
+if not shutil.which("samtools"):
+    log.critical(
+        f"samtools not found in PATH, most (all?) modules will be non-functional."
+    )
